dfe4f90cd710a410ca45ded0384575c202f1bd2a
Rahul750-git <varmarahul08501@gmail.com>
2026-03-10 12:30:56 +0530

    upload files

---
+++ 06 Arithmetic Operator/Arith.js
@@ -0,0 +1,13 @@
+function f1(){
+    alert("Arithmetic Operator");
+    let a = 10;
+    let b = 3;
+    document.writeln(a+b);
+    document.writeln(a-b);
+    document.writeln(a*b);
+    document.writeln(a/b);
+    document.writeln(a%b);
+    document.writeln(a**b);
+    
+    
+}
---
+++ 07 Assignment Operator/Assig.js
@@ -0,0 +1,14 @@
+function f2(){
+    alert("Assignment Operator");
+    let a = 10;
+    let b = 3;
+    a+=b;
+    // a-=b;
+    // a*=b;
+    // a/=b;
+    // a%=b;
+    document.writeln(a)
+
+    
+    
+}
